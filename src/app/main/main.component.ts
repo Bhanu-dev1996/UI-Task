224e7999c4ba0c8775cb2584a609071ee85e8130
Bhanu1796 <58201153+Bhanu1796@users.noreply.github.com>
2024-09-22 00:15:02 +0530

    Initial commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastModule } from 'primeng/toast'; 
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [ToastModule, CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.css',
+  providers: [MessageService] 
+})
+export class MainComponent {
+  isOffcanvasVisible = false;
+  istoggleVisible = true
+  progressValue = 0;
+  intervalId: any;
+
+  constructor(private messageService: MessageService) {}
+
+   // Function to start the migration
+   startMigration() {
+    this.isOffcanvasVisible = true;
+    this.istoggleVisible = true; // Show the offcanvas when start is clicked
+    this.progressValue = 0; // Reset progress value
+
+    // Start the progress bar simulation
+    this.intervalId = setInterval(() => {
+      if (this.progressValue < 100) {
+        this.progressValue += 1;
+      } else {
+        clearInterval(this.intervalId);
+        this.isOffcanvasVisible = false; // Hide offcanvas
+        // Show success toast using PrimeNG's MessageService
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Migration Complete',
+          detail: 'The migration process has been successfully completed!',
+          life: 3000
+        });
+      }
+    }, 100); // Increment every 100ms (this can be adjusted for speed)
+  }
+  
+  
+
+  // Function to toggle the visibility of the offcanvas
+  toggleOffcanvas() {
+    this.isOffcanvasVisible = !this.isOffcanvasVisible;
+  }
+}
